efbc91532e6a4442dcf6aa457530868ca9c96b30
romanssam <troubledyouthh@outlook.com>
2023-09-12 00:13:24 +0500

    module css нужен фикс, не билдится прод css и дев

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,26 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders (): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+    const scssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localeIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
+                    },
+                }
+            },
+            "sass-loader",
+        ],
+    }
+
     const typeScriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -9,5 +29,6 @@ export function buildLoaders (): webpack.RuleSetRule[] {
 
     return [
         typeScriptLoader,
+        scssLoader,
     ]
 }
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import {fn} from "./test";
-
-fn(1111111)
