230ee8e91ba693b96d8e3692dcbcfdce65555f00
romanssam <troubledyouthh@outlook.com>
2023-09-13 00:07:51 +0500

    add code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Counter from "./components/Counter";
+import { Routes, Route, Link } from "react-router-dom";
 import './index.scss'
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
 
 const App = () => {
     return (
-        <div>
-            <Counter />
+        <div className='app'>
+            <Link to={'/'}>Main page</Link>
+            <Link to={'/about'}>About page</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/' element={<MainPage />} />
+                    <Route path='/about' element={<AboutPage />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
