02c775cec75d6d34eb5161c16f69f7b831aa492d
romanssam <troubledyouthh@outlook.com>
2023-09-26 23:59:38 +0500

    сайдбар виджет

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import './styles/index.scss';
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 const App = () => {
     const { theme } = useTheme()
@@ -10,7 +11,10 @@ const App = () => {
     return (
         <div className='app' data-theme={theme}>
             <Navbar />
-            <AppRouter />
+            <div className={'content-page'}>
+                <Sidebar />
+                <AppRouter />
+            </div>
         </div>
     );
 };
