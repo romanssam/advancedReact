8fae3f7be1b5bf3b8f5522a162af4dbbfd2a2060
romanssam <troubledyouthh@outlook.com>
2023-10-24 00:37:10 +0500

    fix absolute path for jest

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,4 +1,4 @@
-import {classNames} from "./classNames";
+import {classNames} from "shared/lib/classNames/classNames";
 
 describe('Хелпер classNames', () => {
     test('Тест с 1 параметром', () => {
