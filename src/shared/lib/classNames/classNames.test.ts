1e5350c708dddbe2f35a578d8a1579545d3f0fff
romanssam <troubledyouthh@outlook.com>
2023-10-24 00:28:03 +0500

    tests for classNames helper

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,7 +1,29 @@
 import {classNames} from "./classNames";
 
-describe('classnames', () => {
-    test('test', () => {
-        expect(true).toBe(true)
+describe('Хелпер classNames', () => {
+    test('Тест с 1 параметром', () => {
+        expect(classNames('someClass', {}, [])).toBe('someClass')
+    })
+    test('Тест с additional', () => {
+        const expected = 'someClass class1 class2'
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected)
+    })
+    test('Тест с mods', () => {
+        const expected = 'someClass class1 class2 hovered scrollable'
+        expect(classNames('someClass',
+            {hovered: true, scrollable: true},
+            ['class1', 'class2'])).toBe(expected)
+    })
+    test('Тест с mods когда он false', () => {
+        const expected = 'someClass class1 class2 hovered'
+        expect(classNames('someClass',
+            {hovered: true, scrollable: false},
+            ['class1', 'class2'])).toBe(expected)
+    })
+    test('Тест с mods когда он undefined', () => {
+        const expected = 'someClass class1 class2 scrollable'
+        expect(classNames('someClass',
+            {hovered: undefined, scrollable: true},
+            ['class1', 'class2'])).toBe(expected)
     })
 });
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -3,8 +3,8 @@ export const classNames = (cls: string, mods?: Mods, additional?: string[]) => {
     return [
         cls,
         ...additional.filter(Boolean),
-        Object.entries(mods)
-            .filter(([className, value]) => Boolean(value))
+        ...Object.entries(mods)
+            .filter(([_, value]) => Boolean(value))
             .map(([className]) => className)
     ]
         .join(' ')
