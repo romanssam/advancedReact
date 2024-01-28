625ffcf190718f3d2059271e0987b6086207e24d
romanssam <troubledyouthh@outlook.com>
2024-01-28 17:07:38 +0500

    shared input

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,14 +1,21 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import styles from './Input.module.scss';
+import {ChangeEvent, InputHTMLAttributes, memo} from "react";
 
-interface InputProps {
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+interface InputProps extends HTMLInputProps{
     className?: string;
+    value?: string;
+    onChange?: (value: string) => void;
     placeholder?: string;
 }
 
-export const Input = (props: InputProps) => {
-    const { className, placeholder } = props
+export const Input = memo((props: InputProps) => {
+    const { className, value, onChange, placeholder, type = 'text', ...other } = props
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        onChange?.(event.target.value);
+    }
     return (
-        <input type="text" className={className} placeholder={placeholder} />
+        <input type={type} className={className} placeholder={placeholder} onChange={onChangeHandler} {...other} />
     );
-};
+});
