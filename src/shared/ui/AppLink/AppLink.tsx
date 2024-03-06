bcfe3c0be9f1a2515a4246a55d211034ceb35140
romanssam <troubledyouthh@outlook.com>
2024-03-06 22:19:35 +0500

    add navLink + cringe styling

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,9 +1,9 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import styles from './AppLink.module.scss';
-import {Link, LinkProps} from "react-router-dom";
+import {NavLinkProps, NavLink} from "react-router-dom";
 import React, {FC, memo, PropsWithChildren} from "react";
 
-interface AppLinkProps extends LinkProps{
+interface AppLinkProps extends NavLinkProps {
     className?: string;
 }
 
@@ -11,12 +11,16 @@ export const AppLink: FC<PropsWithChildren<AppLinkProps>> =  memo((props) => {
     const {to, className, children, ...otherProps} = props
 
     return (
-        <Link
+        <NavLink
             to={to}
-            className={classNames(styles.AppLink, {}, [className])}
+            className={({ isActive}) =>
+                [
+                    isActive ? styles.active : "",
+                    styles.AppLink,
+                ].join(" ")}
             {...otherProps}
         >
             {children}
-        </Link>
+        </NavLink>
     );
 });
